87592f2431e35c1d29e74d1c4235ed52e3af43df
Juan Ignacio Ubeira <jubeira@ekumenlabs.com>
2022-02-21 18:41:25 -0300

    Adding Kovan network as default.

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -80,11 +80,9 @@ module.exports = {
       },
     },
     kovan: {
-      url: "https://kovan.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://kovan.infura.io/v3/c374922eb47a40bfaadaff5eeb1dad7a", // <---- YOUR INFURA ID! (or it won't work)
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/kovan", // <---- YOUR MORALIS ID! (not limited to infura)
-      accounts: {
-        mnemonic: mnemonic(),
-      },
+      accounts: process.env.KOVAN_DEPLOYER_PRIV_KEY !== undefined ? [process.env.KOVAN_DEPLOYER_PRIV_KEY] : [],
     },
     mainnet: {
       url: "https://mainnet.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
